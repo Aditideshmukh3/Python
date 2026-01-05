8990b4665242c8ae4cc3f6519c7206906cf8d869
Aditi Deshmukh <aditideshmukh7301@gmail.com>
2026-01-05 19:54:12 +0530

    This is my Datatypes File

---
+++ datatypes.py
@@ -0,0 +1,43 @@
+#1. integer :
+# a = 10 (int)
+# a = "hello"'hello' (str)
+# a = 12.5 (float)
+# print(a)
+#print(type(a))
+
+#2. boolean :
+# a = True
+# print(type(a))
+
+#3. complex :
+# a = 3+5j
+# #here 3 is real no. and 5 is imaginary no.
+# print(type(a))
+# print("this is imaginary number :" , a.imag)
+# print("this is real number :" , a.real)
+
+#4. list :
+# l = [ 23, 23.4, "hello", 3+5j, True, False, 21 ]
+# print(l)
+# print(type(l))
+
+#5. tuple :
+# l = (10, 20)
+# l = (10, )
+# l = ( 23, 23.4, "hello", 3+5j, True, False, 21)
+# print(l)
+# print(type(l))
+
+#6. set :
+#set cannot be created empty. empty set is dict.
+#set sort the values in output and it does not give duplicate values.
+# value of 1 is also considered as true in output ( same for 0=false).
+# s ={23, 23.4, "hello", 3+5j, True, False, 2, 12, 12}
+# print(s)
+# print(type(s))
+
+#7. dictonary :
+#key:value pair
+# d = {"key":"value","name":"john"}
+# print(d)
+# print(type(d))
