c7596f609d358bdd54e054a45a44be39b6f68a79
Aditi Deshmukh <aditideshmukh7301@gmail.com>
2026-01-06 19:43:14 +0530

    This is my Operators File

---
+++ first.py
@@ -0,0 +1,71 @@
+#Basic datatypes:
+# a = 10 - integer(int)
+# b = 12.3 - float(decimal point)
+# c = "hello" 'hello' - string(str)
+
+#variables :
+# a = b = c = 10
+# print(a)
+# print(b)
+
+#1.	Arithmatic operators :
+# a = 10
+# b = 20
+# print(a * b)
+
+# a = 3
+# b = 2
+# print(a**b)
+# print(b**a)
+# print(a / b)
+# print(a //b)
+# print(a % b)
+
+#2.	Comparison / relational operators :
+# print(10>9)
+# print(10>=9)
+# print(10==9)
+# print(10!=9)
+
+#3.	Logical operators : 
+# a = True
+# b = False
+# print(a and b)
+# print(a or b)
+# print(not a)
+# a = 1
+# b = 0
+# print(a and b)
+# print(a or b)
+
+# print(10 > 9 and 6<10)
+# print(10>9 or 6<10)
+# print(not(10>9 and 6<10))
+
+#4.	Assignment operators : 
+# a = 10
+# print(a)
+# a += 5
+# a -= 5
+# a /= 5
+# a *= 5
+# print(a)
+
+#5.	Membership operators : 
+# a = "Hello"
+# print("h" in a)
+# print("H" in a)
+# print("H"not in a)
+
+#6.	Identity operators : 
+# a = 10
+# b = 20
+# print(a is b)
+# print(a is not b)
+
+#7.	Bitwise operators (works on binary language/number) :
+# a = 12
+# result =a>>2
+# print(result)
+# result =a<<2
+# print(result)
