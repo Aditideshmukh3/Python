b5e1c020b58096d0ad96da43cb7426311e468240
Aditi Deshmukh <aditideshmukh7301@gmail.com>
2026-01-09 22:27:12 +0530

    This is my Random Function File

---
+++ practicerandom.py
@@ -0,0 +1,24 @@
+# import random 
+# from random import *
+# from random import random 
+
+# from random import *
+# # print(random()) - gives random value in output in range 0-1 in decimal form
+# for i in range(5):
+#       print(random())
+
+# from random import *
+# print(randint(0,10)) - gives random value in output in range 0-10 in integer form
+
+# from random import *
+# print(randrange(10,100,5)) - gives random value in output in range 10-100 with step size of 5
+
+# from random import *
+# print(uniform(1,10)) - gives random value in output in range 1-10 in decimal form/ float form
+
+# from random import *
+# l = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+# print(choice(l)) - gives random value from the list
+# print(sample(l, k=2)) - gives random 2 values from the list
+
+ 
