24298cc33300d79a7517adf0726f3d45333d7a75
Aditi Deshmukh <aditideshmukh7301@gmail.com>
2026-01-10 22:04:21 +0530

    This is my Calc Function File

---
+++ calc.py
@@ -0,0 +1,13 @@
+# calcy.py - includes input and output statements
+# calculator.py - includes function definitions/logic
+
+from calculator import *
+
+n1 = int(input("Enter first number : "))
+n2 = int(input("Enter second number : "))   
+
+print("Addition of two numbers :", add(10, 20))
+print("Substraction of two numbers :", sub(10,20))
+print("Division of two numbers :", div(10,20))
+print("Multiplication of two numbers :", mult(10,20))
+
