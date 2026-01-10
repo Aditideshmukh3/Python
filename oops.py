a777c67e1b11aa00c752feb3d8d8097026c7822f
Aditi Deshmukh <aditideshmukh7301@gmail.com>
2026-01-10 22:01:38 +0530

    This is my Oops Function File

---
+++ oops.py
@@ -0,0 +1,33 @@
+# syntax :
+# class class_name:
+#     logic/functions
+
+# c = class_name()
+
+
+# class myclass:
+#     def msg(self):
+#         print("this is my function inside the class")
+#     def hello(self):
+#         print("hello world")
+#     def first(self):
+#         print("good afternoon")
+
+# m = myclass()
+# m.msg() 
+# m.hello()
+# m.first()
+
+
+# class info:
+#     def __init__(self,name,age):
+#         self.name = name
+#         self.age = age
+#         # print("this always executes")
+
+#     def user(self):
+#         print("Name of person : ",self.name)
+#         print("Age of the person : ",self.age)
+
+# i = info("John",21)
+# i.user()
