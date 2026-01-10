d5f7930641751c8aedbe8894323edbe5810267b9
Aditi Deshmukh <aditideshmukh7301@gmail.com>
2026-01-10 22:03:24 +0530

    This is my Tuple Methods File

---
+++ tuplemethods.py
@@ -0,0 +1,20 @@
+# t = () - empty () is tuple
+# t = (12) or ("string") - value in tuple shows output as int or str
+# t = (12,) - ( element + ,) shows output as tuple
+# print(t)
+# print(type(t))
+
+# t = (12, "python", True, 23.4)
+# print(t)
+# print(t[0]) - shows the index value of element in tuple
+# t[0] = 100 - value replacing is not available in tuple as its immutable
+# print(t.index(12)) - shows the index value
+# print(t.count(12)) - shows the count of value
+
+
+# Difference between List & Tuple :
+#             List :                                          Tuple:
+# 1. bracket []                                     1. bracket ()
+# 2. lists are mutable (changes allowed)            2. tuples are immutable (changes not allowed)
+# 3. list can be created with single element        3. tuple can only be created with single element if used comma,
+# 4. multiple methods are available                 4. only two methods are available (index & count)
