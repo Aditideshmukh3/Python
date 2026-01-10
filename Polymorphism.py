1e95d0c7b47146da8400af9dd1840f14015e6087
Aditi Deshmukh <aditideshmukh7301@gmail.com>
2026-01-10 22:02:27 +0530

    This is my Polymorphism File

---
+++ Polymorphism.py
@@ -0,0 +1,34 @@
+# polymorphism : same function name but behaves differently for different objects
+
+# from random import *
+# print([1, 2, 3, 4, 5] * 2)
+# print(5 * 2)
+
+
+# print(len("Python"))
+# print(len([1, 2, 3, 4, 5, "hello"]))
+
+
+# class Cat:
+#     def __init__(self,name):
+#         self.name = name
+
+#     def display(self):
+#         print("Name of the cat is : ",self.name)
+
+# c = Cat("kitty")
+
+# class Dog:
+#     def __init__(self,name):
+#         self.name = name
+
+#     def display(self):
+#         print("Name of the dog is : ",self.name)
+
+# d = Dog("Tiger")
+
+# def info(a):
+#     a.display()
+
+# info(c)
+# info(d)
